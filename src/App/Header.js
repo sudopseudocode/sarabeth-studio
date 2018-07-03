f9794afcaf0f96c7cbe9afc1f94c7e6db9ee29e1
Paul DiLoreto <paul.diloreto@gmail.com>
2018-07-03 01:06:33 -0700

    Fixed Header

--- src/App/Header.js
+++ src/App/Header.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { NavLink } from 'react-router-dom';
 import Hidden from '@material-ui/core/Hidden';
-import Button from '@material-ui/core/Button';
-import MenuIcon from '@material-ui/icons/Menu';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
+import MiniNavigation from './MiniNavigation';
+import Navigation from './Navigation';
 
 class Header extends React.Component {
 	constructor(props) {
@@ -23,12 +20,6 @@ class Header extends React.Component {
 	
 	render() {
 		const { classes, match } = this.props;
-		const NavLinks = [
-			{ label: 'About', path: '/about' },
-			{ label: 'Engagements', path: '/engagements' },
-			{ label: 'Media', path: '/media' },
-			{ label: 'Resume', path: '/resume' }
-		];
 		
 		return (
 			<AppBar position='sticky' className={match.path === '/' && match.isExact ? classes.transparent : classes.appBar}>
@@ -40,90 +31,10 @@ class Header extends React.Component {
 					</Typography>
 					
 					<Hidden smDown>
-						<NavLink to='/about'
-						         className={classes.link}
-						         activeClassName={classes.active}
-						>
-							About
-						</NavLink>
-						
-						<NavLink to='/engagements'
-						         className={classes.link}
-						         activeClassName={classes.active}
-						>
-							Engagements
-						</NavLink>
-						
-						<div aria-owns={this.state.mediaAnchor ? 'Media-Menu' : null}
-						     aria-haspopup="true"
-						     className={classes.mediaLink}
-						>
-							<div className={classes.link}
-							     onClick={event => this.setState({ mediaAnchor: event.currentTarget })}
-							>
-								Media
-							</div>
-							
-							<Menu id='Media-Menu'
-							      classes={{ paper: classes.mediaMenu }}
-							      anchorEl={this.state.mediaAnchor}
-							      open={!!this.state.mediaAnchor}
-							      onClose={() => this.setState({ mediaAnchor: null })}
-							>
-								<NavLink to='/photos'>
-									<MenuItem onClick={() => this.setState({ mediaAnchor: null })}
-									          className={classes.menuLink}
-									>
-										Photos
-									</MenuItem>
-								</NavLink>
-								
-								<Divider className={classes.menuLink} />
-								
-								<NavLink to='/audio'>
-									<MenuItem onClick={() => this.setState({ mediaAnchor: null })}
-									          className={classes.menuLink}
-									>
-										Audio
-									</MenuItem>
-								</NavLink>
-							</Menu>
-						</div>
-						
-						<NavLink to='/resume'
-						         className={classes.link}
-						         activeClassName={classes.active}
-						>
-							Resume
-						</NavLink>
+						<Navigation match={match} />
 					</Hidden>
 					<Hidden mdUp>
-						<Button variant='fab' mini
-						        aria-owns={this.state.menuAnchor ? 'Navigation' : null}
-						        aria-haspopup="true"
-						        color='secondary'
-						        classes={{root: classes.miniMenu}}
-						        onClick={event => this.setState({ menuAnchor: event.currentTarget })}
-						>
-							<MenuIcon />
-						</Button>
-						
-						<Menu id='Navigation'
-						      anchorEl={this.state.menuAnchor}
-						      open={!!this.state.menuAnchor}
-						      onClose={() => this.setState({ menuAnchor: null })}
-						>
-							{NavLinks.map((link, index) => (
-								<NavLink to={link.path}
-								         key={index}
-								         className={classes.menuLink}
-								>
-									<MenuItem onClick={() => this.setState({ menuAnchor: null })}>
-										{link.label}
-									</MenuItem>
-								</NavLink>
-							))}
-						</Menu>
+						<MiniNavigation match={match} />
 					</Hidden>
 				</Toolbar>
 			</AppBar>
@@ -150,37 +61,7 @@ const styles = theme => ({
 			color: 'inherit'
 		}
 	},
-	link: {
-		color: theme.palette.primary.contrastText,
-		textDecoration: 'none',
-		marginLeft: theme.spacing.unit * 4,
-		textTransform: 'uppercase',
-		fontFamily: theme.typography.fontFamily,
-		lineHeight: '1.5rem'
-	},
-	active: {
-		borderBottom: `1px solid`
-	},
-	mediaLink: {
-		cursor: 'pointer'
-	},
-	mediaMenu: {
-		backgroundColor: theme.palette.primary.dark,
-		color: theme.palette.primary.contrastText
-	},
-	miniMenu: {
-		backgroundColor: 'transparent',
-		border: `1px solid ${theme.palette.secondary.main}`,
-		borderRadius: '2px',
-		
-		'&:focus': {
-			backgroundColor: 'transparent'
-		}
-	},
-	menuLink: {
-		color: theme.palette.primary.contrastText,
-		textDecoration: 'none',
-	}
+	
 });
 
 export default withStyles(styles)(Header);
---
+++ src/App/MiniNavigation.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { NavLink } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+
+class MiniNavigation extends React.Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = { menuAnchor: null };
+	}
+	
+	render() {
+		const { classes, match } = this.props;
+		
+		return (
+			<div>
+				<Button variant='fab' mini
+				        aria-owns={this.state.menuAnchor ? 'Navigation' : null}
+				        aria-haspopup="true"
+				        color='secondary'
+				        classes={{root: classes.button}}
+				        onClick={event => this.setState({ menuAnchor: event.currentTarget })}
+				>
+					<MenuIcon />
+				</Button>
+				
+				<Menu id='Navigation'
+				      classes={{ paper: classes.menu }}
+				      anchorEl={this.state.menuAnchor}
+				      open={!!this.state.menuAnchor}
+				      onEnter={() => document.activeElement.blur()}
+				      onClose={() => this.setState({ menuAnchor: null })}
+				>
+					<NavLink to='/about' style={{ textDecoration: 'none' }}>
+						<MenuItem onClick={() => this.setState({ mediaAnchor: null })}
+						          className={classes.menuLink}
+						          selected={match.path === '/about'}
+						>
+							About
+						</MenuItem>
+					</NavLink>
+					
+					<NavLink to='/engagements' style={{ textDecoration: 'none' }}>
+						<MenuItem onClick={() => this.setState({ mediaAnchor: null })}
+						          className={classes.menuLink}
+						          selected={match.path === '/engagements'}
+						>
+							Engagements
+						</MenuItem>
+					</NavLink>
+					
+					<NavLink to='/photos' style={{ textDecoration: 'none' }}>
+						<MenuItem onClick={() => this.setState({ mediaAnchor: null })}
+						          className={classes.menuLink}
+						          selected={match.path === '/photos'}
+						>
+							Photos
+						</MenuItem>
+					</NavLink>
+					
+					<NavLink to='/audio' style={{ textDecoration: 'none' }}>
+						<MenuItem onClick={() => this.setState({ mediaAnchor: null })}
+						          className={classes.menuLink}
+						          selected={match.path === '/audio'}
+						>
+							Audio
+						</MenuItem>
+					</NavLink>
+					
+					<NavLink to='/resume' style={{ textDecoration: 'none' }}>
+						<MenuItem onClick={() => this.setState({ mediaAnchor: null })}
+						          className={classes.menuLink}
+						          selected={match.path === '/resume'}
+						>
+							Resume
+						</MenuItem>
+					</NavLink>
+				</Menu>
+			</div>
+		);
+	}
+}
+
+const styles = theme => ({
+	button: {
+		backgroundColor: 'transparent',
+		border: `1px solid ${theme.palette.secondary.main}`,
+		borderRadius: '2px',
+		
+		'&:focus': {
+			backgroundColor: 'transparent'
+		}
+	},
+	menu: {
+		backgroundColor: theme.palette.primary.dark,
+		color: theme.palette.primary.contrastText
+	},
+	menuLink: {
+		color: theme.palette.primary.contrastText,
+		textTransform: 'uppercase'
+	}
+});
+
+export default withStyles(styles)(MiniNavigation);
