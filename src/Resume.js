da86efe1091b5b54f1fe3d5dfbe6e5ed4e09bc71
Paul DiLoreto <paul.diloreto@gmail.com>
2018-07-11 01:36:04 -0700

    Made PDF responsive

--- src/Resume.js
+++ src/Resume.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import Keys from './keys';
 import Loading from './Loading';
+import detectIt from 'detect-it';
 import { Document, Page } from 'react-pdf/dist/entry.webpack';
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import DownloadIcon from 'mdi-material-ui/CloudDownload';
+
+const NotFound = props => (
+	<div>
+		<Typography variant='headline' color='secondary' gutterBottom>
+			Could not display PDF
+		</Typography>
+		<a href={props.url} className={props.className}>
+			<Typography variant='subtitle' color='inherit'>
+				Download Instead
+			</Typography>
+		</a>
+	</div>
+);
 
 class Resume extends React.Component {
 	constructor(props) {
@@ -10,16 +27,38 @@ class Resume extends React.Component {
 		
 		const Contentful = require('contentful');
 		this.client = Contentful.createClient(Keys);
-		this.state = { loading: true };
+		
+		this.state = {
+			loading: true,
+			width: window.innerWidth
+		};
+		this.updateWidth = this.updateWidth.bind(this);
+	}
+	
+	componentWillMount() {
+		this.updateWidth();
 	}
 	
 	componentDidMount() {
 		this.client.getEntries({ content_type: 'about' }).then(res => {
 			this.setState({
-				resume: res.items[0].fields.resume.fields.file.url,
-				loading: false
+				resume: res.items[0].fields.resume.fields.file.url
 			});
 		});
+		
+		window.addEventListener('resize', this.updateWidth);
+	}
+	
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.updateWidth);
+	}
+	
+	updateWidth() {
+		const maxWidth = 820;
+		const width = window.innerWidth > maxWidth ? maxWidth : (window.innerWidth - 40);
+		
+		if(width !== this.state.width && detectIt.deviceType !== 'touchOnly')
+			this.setState({ width });
 	}
 	
 	render() {
@@ -30,12 +69,26 @@ class Resume extends React.Component {
 				{this.state.loading &&
 					<Loading />
 				}
-				<Document file={this.state.resume}
-				          className={classes.resume}
-				          loading={<Loading />}
-				>
-					<Page pageNumber={1} />
-				</Document>
+				{this.state.resume &&
+					<Document file={this.state.resume}
+					          loading={null}
+					          onLoadSuccess={() => this.setState({ loading: false })}
+					          noData={
+						          <NotFound url={this.state.resume} className={classes.link} />
+					          }
+					>
+							
+							<Page pageNumber={1}
+							      width={this.state.width}
+							>
+								<a href={this.state.resume} className={classes.button}>
+									<Button variant='fab' mini>
+										<DownloadIcon />
+									</Button>
+								</a>
+							</Page>
+					</Document>
+				}
 			</div>
 		);
 	}
@@ -43,7 +96,19 @@ class Resume extends React.Component {
 
 const styles = theme => ({
 	container: {
-		width: '100%'
+		width: '100%',
+		display: 'flex',
+		justifyContent: 'center',
+		paddingTop: theme.spacing.unit * 2
+	},
+	link: {
+		color: theme.palette.primary.contrastText,
+		textAlign: 'center'
+	},
+	button: {
+		position: 'absolute',
+		top: 0, left: 0,
+		margin: theme.spacing.unit
 	},
 	resume: {
 		display: 'flex',
