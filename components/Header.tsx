9893a77421406042e8258a2e372d44e9387ec5d0
Paul DiLoreto <paul.diloreto@gmail.com>
2021-11-07 20:29:21 -0800

    Added mobile navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,43 +1,63 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import LogoSvg from '../public/logo.svg';
-import styles from '../styles/Header.module.css';
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import LogoSvg from "../public/logo.svg";
+import styles from "../styles/Header.module.css";
 
 interface Props {
   brandName: string;
 }
 
 const Header = (props: Props) => {
-  const links: { label: string; url: string; }[] = [
-    { label: 'About', url: '/about' },
-    { label: 'Engagements', url: '/engagements' },
-    { label: 'Media', url: '/media' },
-    { label: 'Lessons', url: '/lessons' },
-    { label: 'Contact', url: '/contact' },
+  const links: { label: string; url: string }[] = [
+    { label: "About", url: "/about" },
+    { label: "Engagements", url: "/engagements" },
+    { label: "Media", url: "/media" },
+    { label: "Lessons", url: "/lessons" },
+    { label: "Contact", url: "/contact" },
   ];
+  const [mobileNavOpen, setMobileNav] = useState(false);
   const router = useRouter();
 
   return (
     <header className={styles.container}>
       <Link href="/">
         <a className={styles.logoContainer}>
-          <span className={styles.logoText}>
-            {props.brandName}
-          </span>
+          <span className={styles.logoText}>{props.brandName}</span>
           <LogoSvg className={styles.logoSvg} />
         </a>
       </Link>
 
-      <div className={styles.linkContainer}>
-        {links.map(link => (
+      <nav
+        className={`${styles.navContainer} ${
+          mobileNavOpen && styles.mobileNavOpen
+        }`}
+      >
+        {links.map((link) => (
           <Link key={`nav-${link.url}`} href={link.url}>
-            <a className={`${link.url === router.route ? styles.activeLink : ''} ${styles.link}`}>
-              {link.label}
+            <a className={styles.linkContainer}>
+              <span
+                className={`${
+                  link.url === router.route ? styles.activeLink : ""
+                } ${styles.link}`}
+              >
+                {link.label}
+              </span>
             </a>
           </Link>
         ))}
-      </div>
+      </nav>
+
+      <button
+        className={styles.mobileNavButton}
+        onClick={() => setMobileNav(!mobileNavOpen)}
+      >
+        <div
+          className={`${styles.hamburgerIcon} ${
+            mobileNavOpen && styles.closeIcon
+          }`}
+        />
+      </button>
     </header>
   );
 };
