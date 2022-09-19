ecdcd3db6577a3f9317a4e30aafd624e27c84161
Paul DiLoreto <paul.diloreto@gmail.com>
2022-09-18 17:46:08 -0700

    feat(): BannerImage uses imageLoader

--- components/BannerImage/index.tsx
+++ components/BannerImage/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Image } from "../../utils/contentful";
+import { imageLoader } from "../../utils/contentful";
 import Overlay from "../Overlay";
 import styles from "./BannerImage.module.scss";
+import type { Image } from "../../utils/contentful";
 
 type Props = {
   image: Image;
@@ -12,7 +13,12 @@ const BannerImage = ({ title, image }: Props) => {
   return (
     <div
       className={styles.container}
-      style={{ backgroundImage: `url("${image.url}")` }}
+      style={{
+        backgroundImage: `url("${imageLoader({
+          src: image.url,
+          width: image.width,
+        })}")`,
+      }}
     >
       <Overlay type="bannerTitle" direction="left">
         <h1 className={styles.text}>{title}</h1>
