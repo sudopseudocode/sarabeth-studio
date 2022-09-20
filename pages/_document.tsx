63475bd401ceff3f20bb6f69a9bbca00c4772013
Paul DiLoreto <paul.diloreto@gmail.com>
2022-09-20 13:31:49 -0700

    fix(): Google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,8 @@
 import { Head, Html, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script strategy="lazyOnload" id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-          page_path: window.location.pathname,
-          });
-        `}
-      </Script>
-
       <Head />
       <body>
         <Main />
