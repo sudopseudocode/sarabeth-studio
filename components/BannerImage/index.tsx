3d20b571f60ec71edb63cbf00822c3d8354f0f8d
Paul DiLoreto <paul.diloreto@gmail.com>
2022-09-20 22:06:27 -0700

    feat(): image optimization on BannerImage

--- components/BannerImage/index.tsx
+++ components/BannerImage/index.tsx
@@ -1,4 +1,6 @@
+import NextImage from "next/image";
 import React from "react";
+import { imageLoader } from "../../utils/client/contentful";
 import Overlay from "../Overlay";
 import styles from "./BannerImage.module.scss";
 import type { Image } from "../../utils/server/contentful";
@@ -10,10 +12,19 @@ type Props = {
 
 const BannerImage = ({ title, image }: Props) => {
   return (
-    <div
-      className={styles.container}
-      style={{ backgroundImage: `url("${image.url}")` }}
-    >
+    <div className={styles.container}>
+      <NextImage
+        className={styles.image}
+        loader={imageLoader}
+        placeholder="blur"
+        blurDataURL={image.blurDataUrl}
+        src={image.url}
+        alt={image.description}
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+        sizes="100vw"
+      />
       <Overlay type="bannerTitle" direction="left">
         <h1 className={styles.text}>{title}</h1>
       </Overlay>
