372b067cb48d9a4f15cfdb436c78b22c36de0eda
Paul DiLoreto <paul.diloreto@gmail.com>
2022-10-03 03:04:28 -0700

    feat(): add favicon

--- components/PageLayout/index.tsx
+++ components/PageLayout/index.tsx
@@ -38,6 +38,33 @@ const PageLayout = ({ metadata, commonData, children }: Props) => {
         <meta name="GOOGLEBOT" content="index, follow" />
         <meta name="ROBOTS" content={metadata.robots || "index, follow"} />
         <meta name="robots" content={metadata.robots || "index, follow"} />
+
+        {/* Favicon */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <Header brandName={commonData.brandName} />
